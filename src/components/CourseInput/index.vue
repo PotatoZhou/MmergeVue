a784cf40728c93d7945b0f2650f80701cca23793
Depressedon9MUA <g46zhou@uwaterloo.ca>
2023-09-27 15:43:25 -0400

    update tabplane

--- src/components/CourseInput/index.vue
+++ src/components/CourseInput/index.vue
@@ -1,20 +1,3 @@
 <template>
-  <div class="header">
-    <el-select
-      v-model="value"
-      multiple
-      filterable
-      allow-create
-      default-first-option
-      placeholder="Choose tags for your article"
-    >
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-      >
-      </el-option>
-    </el-select>
-  </div>
+  <div class="header">hello</div>
 </template>
