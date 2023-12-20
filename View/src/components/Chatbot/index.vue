b79d41da8c29c321e992ddb395004972f3c95d86
Depressedon9MUA <g46zhou@uwaterloo.ca>
2023-12-19 19:17:55 -0500

    using cytoscape to display course info as graph

--- View/src/components/Chatbot/index.vue
+++ View/src/components/Chatbot/index.vue
@@ -1,3 +1,60 @@
 <template>
-  <div class="header">Chatbot</div>
+  <div class="header">CourseGraph</div>
+  <cytoscape
+  ref="cy"
+  :config="config"
+  v-on:tap="ShowInfo"
+  >
+  <cy-element
+    v-for="def in elements"
+    :key="`${def.data.id}`"
+    :definition="def"
+    v-on:tap="ShowInfo($event, def.data.id)"
+  />
+</cytoscape>
 </template>
+
+<script>
+import CyElement from '@/components/CyElement';
+import { CytoscapeOptions } from 'cytoscape';
+import VueCytoscape from 'vue-cytoscape';
+export default {
+  components: {
+    VueCytoscape,
+    CyElement
+  },
+
+  data() {
+    return {
+      config: CytoscapeOptions = {
+          elements: [
+            { data: { id: 'a' } },
+            { data: { id: 'b' } },
+            {
+              data: {
+                id: 'ab',
+                source: 'a',
+                target: 'b'
+              }
+            }
+          ],
+      }
+    };
+  },
+
+  created() {
+    console.log('created')
+    importCourse();
+  },
+  
+  methods: {
+    importCourse() {
+      console.log('importCourse')
+      this.$http.get('http://localhost:5000/api/course').then((response) => {
+        this.CourseData = response.data;
+        console.log(this.CourseData);
+      });
+    }
+  }
+}
+</script>
---
+++ View/src/components/test/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+      <Cytoscape ref="cy" :config="config" v-on:mousedown="addNode" v-on:cxttapstart="updateNode">
+        <cy-element
+          v-for="def in elements"
+          :key="`${def.data.id}`"
+          :definition="def"
+          v-on:mousedown="deleteNode($event, def.data.id)"
+        />
+      </Cytoscape>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import CyElement from '@/components/CyElement'
+import Cytoscape from '@/components/Cytoscape'
+import config from '@/example-config.json'
+import { EventObject } from 'cytoscape'
+import { Component, Vue } from 'vue-property-decorator'
+  
+  @Component({
+    components: {
+      Cytoscape,
+      CyElement
+    }
+  })
+  export default class App extends Vue {
+    addNode(event: EventObject) {
+      if (event.target === (this.$refs.cy as Cytoscape).instance)
+        console.log('adding node', event)
+    }
+  
+    deleteNode(event: Event, id: string) {
+      console.log('node clicked', id)
+    }
+  
+    updateNode(event: any) {
+      console.log('right click node', event)
+    }
+  
+    get elements() {
+      return config.elements
+    }
+  
+    get config() {
+      const noElementsConfig = { ...config }
+      delete noElementsConfig.elements
+      return noElementsConfig
+    }
+  }
+  </script>
+  <style>
+  #app {
+    width: 100%;
+    height: 400px;
+  }
+  </style>
+  
+  
