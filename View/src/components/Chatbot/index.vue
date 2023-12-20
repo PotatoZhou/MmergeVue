090efd229e4e6538a4ffac905a309faa99f3524a
Depressedon9MUA <g46zhou@uwaterloo.ca>
2023-12-19 23:43:13 -0500

    delete unused files, adding eslint

--- View/src/components/Chatbot/index.vue
+++ View/src/components/Chatbot/index.vue
@@ -1,60 +1,56 @@
 <template>
-  <div class="header">CourseGraph</div>
-  <cytoscape
-  ref="cy"
-  :config="config"
-  v-on:tap="ShowInfo"
-  >
-  <cy-element
-    v-for="def in elements"
-    :key="`${def.data.id}`"
-    :definition="def"
-    v-on:tap="ShowInfo($event, def.data.id)"
-  />
-</cytoscape>
+  <div class="main">
+    <cytoscape ref="cy" :config="config" v-on:tap="ShowInfo">
+      <cy-element
+        v-for="def in elements"
+        :key="`${def.data.id}`"
+        :definition="def"
+        v-on:tap="ShowInfo($event, def.data.id)"
+      />
+    </cytoscape>
+  </div>
 </template>
 
 <script>
-import CyElement from '@/components/CyElement';
-import { CytoscapeOptions } from 'cytoscape';
-import VueCytoscape from 'vue-cytoscape';
+import VueCytoscape from "vue-cytoscape";
 export default {
   components: {
     VueCytoscape,
-    CyElement
   },
 
   data() {
     return {
-      config: CytoscapeOptions = {
-          elements: [
-            { data: { id: 'a' } },
-            { data: { id: 'b' } },
-            {
-              data: {
-                id: 'ab',
-                source: 'a',
-                target: 'b'
-              }
-            }
-          ],
-      }
+      config: (CytoscapeOptions = {
+        elements: [
+          { data: { id: "a" } },
+          { data: { id: "b" } },
+          {
+            data: {
+              id: "ab",
+              source: "a",
+              target: "b",
+            },
+          },
+        ],
+      }),
     };
   },
 
   created() {
-    console.log('created')
+    console.log("created");
     importCourse();
   },
-  
+
   methods: {
     importCourse() {
-      console.log('importCourse')
-      this.$http.get('http://localhost:5000/api/course').then((response) => {
-        this.CourseData = response.data;
-        console.log(this.CourseData);
-      });
-    }
-  }
-}
+      console.log("importCourse");
+    },
+
+    ShowInfo(event, id) {
+      console.log("ShowInfo");
+      console.log(event);
+      console.log(id);
+    },
+  },
+};
 </script>
